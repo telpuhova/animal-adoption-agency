7584cd852d88cfe463b83aa472a4d089b898153b
ross fletcher and zach evans <zach@>
2017-12-18 10:13:52 -0800

    Add pet submission form feature.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//front-end ---------------------------
 function Animal(type, name, age) {
   this.type = type;
   this.name = name;
@@ -8,3 +9,17 @@ function Animal(type, name, age) {
 Animal.prototype.setAdopted = function () {
   this.adopted = true;
 };
+
+//back-end ----------------------------
+
+$(document).ready(function() {
+  $("#adopt-form").submit(function(event) {
+    event.preventDefault();
+    var type = $("#pet-type").val();
+    var name = $("#pet-name").val();
+    var age = $("#age").val();
+    var newAnimal = new Animal(type, name, age);
+    $("#available").append("<ul>" + newAnimal.name + "</ul>" + "<li>" + newAnimal.type + "</li>" + "<li>" + newAnimal.age + "</li>" );
+
+  })
+});
