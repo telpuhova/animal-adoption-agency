de0bb4665026c058b70daf12db79287a6dd6cbfc
telpuhova <telpuhova@gmail.com>
2017-12-18 13:14:17 -0800

    add ability to add image

--- js/scripts.js
+++ js/scripts.js
@@ -12,26 +12,38 @@ Animal.prototype.setAdopted = function () {
 
 //front-end ----------------------------
 
+function readURL(input) {
+  debugger;
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    // reader.onload = function (e) {
+    //   $('#blah')
+    //     .attr('src', e.target.result)
+    //     .width(150)
+    //     .height(200);
+    // };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
 $(document).ready(function() {
   $("#adopt-form").submit(function(event) {
     event.preventDefault();
     var type = $("#pet-type").val();
     var name = $("#pet-name").val();
     var age = $("#age").val();
+    // var pic = $("#file").val();
+    var pic = $("#pic").val();
     var newAnimal = new Animal(type, name, age);
 
-    $("#available").append("<li class='unadopted entry'>" + newAnimal.name + "<ul class='details'><li>" + newAnimal.type + "</li><li>" + newAnimal.age + " years old</li></ul></li>");
-
-    // $("#available").append("<li class='unadopted entry'> <p>hi</p><p>Hey</p></li>");
+    $("#available").append("<li class='unadopted entry'>" + newAnimal.name + "<ul class='details'><li>" + newAnimal.type + "</li><li>" + newAnimal.age + " years old</li><li><img src='" + pic + "' alt='doggy'></li></ul></li>");
 
     $(".details").hide();
-    // $(".hide").hide();
 
     $(".entry").last().click(function() {
-      // alert(this.next);
-      // $(this.child.next).show();
       $(this).find("ul").toggle();
-    })
+    });
+
 
   });
 });
