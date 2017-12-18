8ec38df50dda01270bbca94c3397af63fd709590
telpuhova <telpuhova@gmail.com>
2017-12-18 10:07:30 -0800

    add and display pets

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//back end
 function Animal(type, name, age) {
   this.type = type;
   this.name = name;
@@ -8,3 +9,16 @@ function Animal(type, name, age) {
 Animal.prototype.setAdopted = function () {
   this.adopted = true;
 };
+
+//front end
+$(document).ready(function() {
+  $("#adoptForm").submit(function(event) {
+    event.preventDefault();
+    var type = $("#pet-type").val();
+    var name = $("#pet-name").val();
+    var age = $("#age").val();
+    var newAnimal = new Animal(type, name, age);
+
+    $("#available").append("<li>" + newAnimal.type + "<br>" + newAnimal.name + "<br>" + newAnimal.age + "</li>")
+  });
+});
